b764db3d046754d0a8401a0f26cee4ddd3f68b9e
Joshua Wheeler <joshua.r.wheeler.27@gmail.com>
2021-12-13 00:09:13 -0600

    Updated workbook, added modules and report

---
+++ statistics.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from scipy import stats
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def t_test_sqft_tax(train):
+    '''
+    Takes in train df as argument and runs an independent t-test for 
+    first and third quartile calculated square feet and their tax value
+    '''
+    # establish alpha
+    alpha = 0.5
+    # create variables for 3rd and 1st quartile
+    highsqf = train[train.calculatedfinishedsquarefeet >= train.calculatedfinishedsquarefeet.quantile(q=0.75)].taxvaluedollarcnt
+    lowsqf = train[train.calculatedfinishedsquarefeet <= train.calculatedfinishedsquarefeet.quantile(q=0.25)].taxvaluedollarcnt
+    # run test
+    t, p = stats.ttest_ind(highsqf, lowsqf, equal_var=False)
+
+    # return result
+    if p / 2 > alpha:
+        return print("We fail to reject the null hypothesis")
+    elif t < 0:
+        return print("We fail to reject the null hypothesis")
+    else:
+        return print("Higher square footage properties generally have a higher calculated tax amount.")
+
+def t_test_bath_tax(train):
+    '''
+    Takes in train df as argument and calculates the mean taxvalue for each number category of bathrooms
+    and runs an independent t-test for 1-2 bathrooms and 3-4 bathrooms tax values.
+    '''
+    # establish alpha
+    alpha = 0.5
+    # create variables for 1 and 2 baths and 3 and 4 baths
+    one_two_bath = train[train.bathroomcnt <= 2].taxvaluedollarcnt
+    three_four_bath = train[train.bathroomcnt >= 3 ].taxvaluedollarcnt
+    # run test
+    t, p = stats.ttest_ind(one_two_bath, three_four_bath, equal_var=False)
+     # return result
+    if p / 2 > alpha:
+        return  print("There is no statistical evidence that more bathrooms have higher tax values")
+    elif t < 0:
+        return  print("There is no statistical evidence that more bathrooms have higher tax values")
+    else:
+        return  print("Higher bathroom count properties generally have a higher calculated tax amount.")
+
+def t_test_bed_tax(train):
+    '''
+    Takes in train df as argument and calculates the mean taxvalue for each number category of bathrooms
+    and runs an independent t-test for 1-2 bedrooms and 3-4 bedrooms tax values.
+    '''
+    # establish alpha
+    alpha = 0.5
+    # create variables for 1 and 2 baths and 3 and 4 baths
+    two_bed = train[train.bedroomcnt == 2].taxvaluedollarcnt
+    five_bed = train[train.bedroomcnt == 5].taxvaluedollarcnt
+    # run test
+    t, p = stats.ttest_ind(two_bed, five_bed, equal_var=False)
+     # return result
+    if p / 2 > alpha:
+        return  print("There is no statistical evidence that more bedrooms have higher tax values")
+    elif t < 0:
+        return  print("There is no statistical evidence that more bedrooms have higher tax values")
+    else:
+        return  print("Higher bedroom count properties generally have a higher calculated tax amount.")
+
+def test_lowbed_highsqf(train):
+    '''
+    Takes in train df as an argument and returns the means of low bedroom count and high sqft
+    and high bedroom count and low square feet.
+    '''
+    # calculate means for categories
+    mean1 = round(train[(train.bedroomcnt < 3) & (train.calculatedfinishedsquarefeet > 2000)].taxvaluedollarcnt.mean())
+    mean2 = round(train[(train.bedroomcnt >= 3) & (train.calculatedfinishedsquarefeet < 2000)].taxvaluedollarcnt.mean())
+    # create temporary df for plotting purposes
+    temp = train[(train.bedroomcnt <= 3) & (train.calculatedfinishedsquarefeet > 2000)]
+    temp2 = train[(train.bedroomcnt > 3) & (train.calculatedfinishedsquarefeet < 2000)]
+    
+    #create plot
+    plt.figure(figsize=(10,10))
+    plt.subplot(1,2,1)
+    sns.boxplot(x='bedroomcnt',y='taxvaluedollarcnt',data=temp)
+    plt.title('Less than 4 Bedrooms and More than 2k SqFt')
+    plt.subplot(1,2,2)
+    sns.boxplot(x='bedroomcnt',y='taxvaluedollarcnt',data=temp2)
+    plt.title('More than 3 bedrooms and Less than 2k SqFt');
+    
+    # return means and plot
+    return print(f'The mean tax value for properties with less than three bedrooms and more than 2000 square feet is {mean1}, \nThe mean tax value for properties with three or more bedrooms and less than 2000 square feet is {mean2}.'), plt.show()
+
+def test_year_built(train):
+    '''
+    Takes in train df as an argument and runs a chi2 test to find correlation between yearbuilt and taxvalue
+    Also plots a linepot to show taxvalue relating to year the property was built.
+    '''
+    # establish alpha
+    alpha = 0.5
+    # create crosstab observation
+    observe = pd.crosstab(train.taxvaluedollarcnt, train.yearbuilt)
+    # run chi2 test
+    chi2, p, degf, expected = stats.chi2_contingency(observe)
+    # create lineplot
+    sns.lineplot(x = 'yearbuilt',y='taxvaluedollarcnt',data=train)
+    plt.title('Tax Value by Year Built')
+    plt.ylabel('tax value')
+    plt.xlabel('year built')
+    # return result based on p
+    if p < alpha:
+        return print("There is a correlation between year built and tax value."), plt.show()
+    else:
+        return print("There is no correlation between year built and tax value."), plt.show()
+
+def test_fips(train):
+    '''
+    Takes train df as argument and runs chi2 test on fips and returns outcome
+    '''
+    # establish alpha
+    alpha = 0.5
+    # create observation crosstab
+    observe = pd.crosstab(train.taxvaluedollarcnt, train.fips)
+    # run chi2 test
+    chi2, p, degf, expected = stats.chi2_contingency(observe)
+    # return result
+    if p < alpha:
+        return print("There is a correlation between fips and tax value.")
+    else:
+        return print("There is no correlation between fips and tax value.")
---
+++ viz.py
@@ -0,0 +1,73 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def baseline_viz(y_train, pred_mean):
+    '''
+    Plots a histogram of the actual tax value and the baseline prediction
+    '''
+    plt.vlines(ymin = 0, ymax = 25000, x = pred_mean, color='red', alpha=.5)
+
+    plt.hist(y_train.taxvaluedollarcnt, color='blue', alpha=.5, label="Actual Tax Value")
+    plt.hist(y_train.baseline_pred_mean, bins=1, color='red', alpha=.5, rwidth=100, label="Predicted Tax Value - Mean")
+
+    plt.title('Baseline Prediction ')
+    plt.xlabel('Tax Value')
+    plt.ylabel('Number of Houses')
+    plt.legend()
+    return plt.show()
+
+def validate_scatter(y_validate, pred_mean):
+    '''
+    Creates scatterplot to display models and baseline 
+    '''
+    plt.figure(figsize=(16,8))
+    plt.plot(y_validate.taxvaluedollarcnt, y_validate.baseline_pred_mean, alpha=.5, color="black", label='_nolegend_')
+    plt.annotate("Baseline: Predict Using Mean", (16, (pred_mean + 1000)))
+    plt.plot(y_validate.taxvaluedollarcnt, y_validate.taxvaluedollarcnt, alpha=.5, color="blue", label='_nolegend_')
+    plt.annotate("The Ideal Line: Predicted = Actual", (30000, 0), rotation=25)
+
+    plt.scatter(y_validate.taxvaluedollarcnt, y_validate.taxvalue_pred_lm, 
+                alpha=.5, color="red", s=100, label="Model: LinearRegression")
+    plt.scatter(y_validate.taxvaluedollarcnt, y_validate.taxvalue_pred_lm2, 
+                alpha=.5, color="yellow", s=100, label="Model: Polynomial")
+    plt.scatter(y_validate.taxvaluedollarcnt, y_validate.taxvalue_pred_lars, 
+                alpha=.5, color="green", s=100, label="Model LassoLars")
+    plt.xlabel('Tax Value')
+    plt.ylabel('Model Predictions')
+    plt.legend()
+    return plt.show()
+
+def plot_residuals(y_validate):
+    plt.figure(figsize=(16,8))
+    plt.axhline(label="No Error")
+    plt.scatter(y_validate.taxvaluedollarcnt, y_validate.taxvalue_pred_lm-y_validate.taxvaluedollarcnt, 
+                alpha=.5, color="red", s=100, label="Model: LinearRegression")
+    plt.scatter(y_validate.taxvaluedollarcnt, y_validate.taxvalue_pred_glm-y_validate.taxvaluedollarcnt, 
+                alpha=.5, color="yellow", s=100, label="Model: Polynomial")
+    plt.scatter(y_validate.taxvaluedollarcnt, y_validate.taxvalue_pred_lars-y_validate.taxvaluedollarcnt, 
+                alpha=.5, color="green", s=100, label="Model LassoLars")
+    plt.legend()
+    plt.xlabel("Actual Tax Value")
+    plt.ylabel("Residuals")
+    plt.title("Do the size of errors change as the actual value changes?")
+    return plt.show()
+
+def validate_hist(y_validate):
+    '''
+    Creates histogram to display models vs actual
+    '''
+    plt.figure(figsize=(10,10))
+    plt.hist(y_validate.taxvaluedollarcnt, color = 'blue',alpha = 0.5, label = 'Actual Tax Value')
+    plt.hist(y_validate.taxvalue_pred_lm, color = 'red',alpha = 0.5, label = 'Model: Linear Regression')
+    plt.hist(y_validate.taxvalue_pred_lm2, color = 'yellow',alpha = 0.5, label = 'Model : Polynomial')
+    plt.hist(y_validate.taxvalue_pred_lars, color = 'green',alpha=0.5,label='Model: LassoLars')
+    plt.xlabel('Tax Value')
+    plt.ylabel('Number of Houses')
+    plt.legend()
+    return plt.show()
+
+def test_hist(y_test):
+    plt.figure(figsize=(10,10))
+    plt.hist(y_test.taxvaluedollarcnt, color = 'blue',alpha = 0.5, label = 'Actual Tax Value')
+    plt.hist(y_test.taxvalue_pred_lm2, color = 'yellow',alpha=0.5,label='Model: Polynomial')
+    return plt.show()
